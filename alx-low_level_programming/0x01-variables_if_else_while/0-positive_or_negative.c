2c8455ad37b031a4e3ecd28e56511f043961856d
aissam0011 <aissam.alaarabiou@gmail.com>
2023-06-16 10:22:19 -0700

    betty 0-positive_or_negative.c

--- alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
+++ alx-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,21 +8,20 @@
 */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-        if (n < 0)
-        {
-               printf("%d is %s\n", n, "negative");
-        }
-        else if (n > 0)
-        {
-               printf("%d is %s\n", n, "positive");
-        }
-        else if (n == 0) 
-        {
-               printf("%d is %s\n", n, "zero");
-        }
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n < 0)
+{
+printf("%d is %s\n", n, "negative");
+}
+else if (n > 0)
+{
+printf("%d is %s\n", n, "positive");
+}
+else 
+{
+printf("%d is %s\n", n, "zero");
+}
+return (0);
 }
